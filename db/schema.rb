e19420d1fa15c9978beec302cd0f63e2c5c4d015
utindenis <denis.u.workk@gmail.com>
2021-07-09 04:54:54 +0400

    Change gem pg and add ruby version.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20200817114526) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
